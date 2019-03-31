7679ee4f5784597320d0f8b35edd264d7ca9fc05
Jakub Półgęsek <jakub@polgesek.pl>
2019-03-31 21:19:55 +0200

    Fix formatting in AppConfig

--- train/AppConfig.h
+++ train/AppConfig.h
@@ -1,15 +1,15 @@
 #include "Secrets.h" // For storing WiFi SSIDs and passwords
 
-#define SERIAL_BAUD 250000
+#define SERIAL_BAUD 			250000
 
 // Network config
-#define HOSTNAME "webtrain1"
-#define DEFAULT_SERVER_IP "10.0.0.201"
+#define HOSTNAME 				"webtrain1"
+#define DEFAULT_SERVER_IP 		"10.0.0.201"
 #define DEFAULT_SERVER_API_PATH "train_api"
 #define APMODE
 
-#define HTTP_TIMEOUT 300
-#define JSON_BUFFER  430
+#define HTTP_TIMEOUT 			300
+#define JSON_BUFFER  			430
 
 // Update config
 #define UPDATE_PATH 			"/update"
@@ -31,7 +31,7 @@
 #define MAIN_TICK_DELAY 5000 // [ns]
 
 
-/* DO NOT CHANGE THIS */
+// DO NOT CHANGE THIS
 #ifndef CONFIG_LOADED
 #define CONFIG_LOADED
 String SERVER_IP = DEFAULT_SERVER_IP;
